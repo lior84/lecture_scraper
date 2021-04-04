726b3e3a6ce884bb8498dd9718bc645035543d0e
lior84 <liorhar84@gmail.com>
2021-04-04 13:11:41 +0300

    Saving the data in json file and gets accecible url which does not require authentication

--- Scrapper.py
+++ Scrapper.py
@@ -1,5 +1,13 @@
 import time as t
 from selenium import webdriver
+import json
+from datetime import datetime
+import os
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
 
 driver = webdriver.Chrome('C:/Users/L879444/Downloads/chromedriver.exe')
 
@@ -10,8 +18,8 @@ driver.get("https://moodle2.bgu.ac.il/moodle/local/mydashboard/")
 id_box = driver.find_element_by_id('login_username')
 pass_box = driver.find_element_by_id('login_password')
 
-username = ''
-password = ''
+username = "liorhari"
+password = "qaz1299Wsx"
 #Type into the boxes
 id_box.send_keys(username)
 pass_box.send_keys(password)
@@ -34,11 +42,12 @@ for i in range(5):
 
 
 class video:
-    def __init__(self, groupNum, date, lecturer, source):
+    def __init__(self, groupNum, date, lecturer, source, videoUrl):
         self.groupNum = groupNum
         self.date = date
         self.lecturer = lecturer
         self.source = source
+        self.videoUrl = videoUrl
 
 # creating list
 listOfGroupNames = []
@@ -64,6 +73,44 @@ for item in items:
     #
     #     listOfGroupNames.append( groupNumSeg[:3] )
     #     print(dateSeg)
-    listOfGroupNames.append( video(str(groupNumSeg[:3]), str(dateSeg), str(lecturerSeg.get_attribute("innerHTML")), str(sourceSeg))  )
+
+    listOfGroupNames.append( video(str(groupNumSeg[:3]), str(dateSeg), str(lecturerSeg.get_attribute("innerHTML")), str(sourceSeg), ""))
+
+#create json file to store the data
+Principles_of_Programming_Languages = []
+
+#going to the source URL so the video could be downloaded
 for obj in listOfGroupNames:
-    print(obj.groupNum + ", " + obj.lecturer + ", " + obj.date + ", " + obj.source + "\n" )
+    driver.get(obj.source)
+    driver.switch_to.frame("kplayer_ifp")
+    videoUrl = str(driver.page_source).split("dataUrl\":\"")[1].split("/https")[0]
+    obj.videoUrl = videoUrl
+    driver.get(videoUrl)
+    Principles_of_Programming_Languages.append({
+        'group_number' : obj.groupNum,
+        'date' : obj.date,
+        'lecturer' : obj.lecturer,
+        'video_url' : obj.videoUrl
+    })
+
+with open(os.path.join('../ZooMoodle/src/jsonFiles', 'Principles_of_Programming_Languages.json'), 'w') as outfile:
+    json.dump(Principles_of_Programming_Languages, outfile)
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+
+# def download_file(url):
+#     local_filename = url.split('/')[-1]
+#     # NOTE the stream=True parameter
+#     r = requests.get(url, stream=True)
+#     with open(local_filename, 'wb') as f:
+#         for chunk in r.iter_content(chunk_size=1024):
+#             if chunk: # filter out keep-alive new chunks
+#                 f.write(chunk)
+#                 #f.flush() commented by recommendation from J.F.Sebastian
+#     return local_filename
+#
+# download_file("http://www.jpopsuki.tv/images/media/eec457785fba1b9bb35481f438cf35a7_1351466328.mp4")
